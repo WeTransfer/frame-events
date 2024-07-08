5cf586a7249fa1dda8bc5b3a996ea6f3c15a2d2a
Oluwafemi Akinde <oluwafemi.akinde@wetransfer.com>
2024-07-08 17:21:34 +0100

    chore(frame-events): add jsdoc to event emitter

--- src/helpers/event-emitter.ts
+++ src/helpers/event-emitter.ts
@@ -1,10 +1,29 @@
+/**
+ *
+ * A callback function that handles emitted events.
+ * @param args - The arguments passed to the callback function.
+ * @interface SubscriberCallback
+ */
 export interface SubscriberCallback {
-  (...args: unknown[]): void;
+  (...args: any[]): void;
 }
+
+/**
+ *
+ * An object containing subscribers for different events.
+ * @interface Subscribers
+ */
 export interface Subscribers {
   [key: string]: SubscriberCallback[];
 }
 
+/**
+ *
+ * A class for emitting and subscribing to events.
+ *
+ * @export
+ * @class Events
+ */
 export default class Events {
   readonly subscribers: Subscribers;
 
@@ -12,24 +31,40 @@ export default class Events {
     this.subscribers = {};
   }
 
+  /**
+   * Handles subscribing to events by adding a callback to the subscribers array under the specified event.
+   *
+   * @param {string} event - The event to subscribe to.
+   * @param {SubscriberCallback} callback - The callback function to be executed when the event is emitted.
+   * @return {object} An object with an `off` method to unsubscribe the callback from the event.
+   */
   on(event: string, callback: SubscriberCallback) {
     if (!this.subscribers[event]) {
       this.subscribers[event] = [];
     }
 
-    const index = this.subscribers[event].push(callback) - 1;
+    this.subscribers[event].push(callback);
 
     return {
       off: () => {
-        this.subscribers[event].splice(index, 1);
+        this.subscribers[event] = this.subscribers[event].filter(
+          (sub) => sub !== callback
+        );
       },
     };
   }
 
-  emit(event: string, data: unknown) {
+  /**
+   * Emits an event and executes all callbacks associated with it.
+   *
+   * @param {string} event - The event to be emitted.
+   * @param {...any[]} args - The arguments to be passed to the callbacks.
+   * @return {void}
+   */
+  emit(event: string, ...args: any[]) {
     if (!this.subscribers[event]) return;
     this.subscribers[event].forEach((subscriberCallback: SubscriberCallback) =>
-      subscriberCallback(data)
+      subscriberCallback(...args)
     );
   }
 }
